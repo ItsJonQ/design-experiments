083263cc95947a5ec9ad6baf7fe28e2fa54b4a9e
Jon Q <hello@jonquach.com>
2019-11-08 11:14:42 -0500

    Add computed color support

--- js/index.js
+++ js/index.js
@@ -1,63 +1,90 @@
-// (function() {
-// 	const createInput = ({ label, prop, value }) => {
-// 		const formControl = document.createElement('div');
-// 		formControl.style.marginBottom = '10px';
+(function() {
+	/**
+	 * Util function for lightening the colour with a %
+	 * @param - string - colour with leading #
+	 * @param - number - percentage to lighten by
+	 */
+	function lighten(c, p) {
+		let n = parseInt(c.slice(1), 16),
+			a = Math.round(2.55 * p),
+			// Bitshift 16 bits to the left
+			r = (n >> 16) + a,
+			// Bitshift 8 bits to the left based on blue
+			b = ((n >> 8) & 0x00ff) + a,
+			//
+			g = (n & 0x0000ff) + a;
+		// Calculate
+		return (
+			'#' +
+			(
+				0x1000000 +
+				(r < 255 ? (r < 1 ? 0 : r) : 255) * 0x10000 +
+				(b < 255 ? (b < 1 ? 0 : b) : 255) * 0x100 +
+				(g < 255 ? (g < 1 ? 0 : g) : 255)
+			)
+				.toString(16)
+				.slice(1)
+		);
+	}
 
-// 		const labelNode = document.createElement('label');
-// 		labelNode.innerHTML = label;
+	function getCurrentPrimaryColor() {
+		const styles = window.getComputedStyle(document.documentElement);
+		const primaryColor = styles.getPropertyValue('--wp-ui--color-primary');
 
-// 		const input = document.createElement('input');
-// 		input.setAttribute('type', 'text');
-// 		input.setAttribute('value', value);
-// 		input.style.maxWidth = '100%';
+		return primaryColor;
+	}
 
-// 		input.onchange = (event) => {
-// 			const root = document.documentElement;
-// 			root.style.setProperty(`--wpcui-${prop}`, event.target.value);
-// 		};
+	function setCssVariable(prop, value) {
+		document.documentElement.style.setProperty(prop, value);
+	}
 
-// 		labelNode.appendChild(input);
-// 		formControl.appendChild(labelNode);
+	function setColorVariants() {
+		const primaryColor = getCurrentPrimaryColor();
 
-// 		return formControl;
-// 	};
+		const values = primaryColor.replace('#', '');
+		const color5 = lighten(values, 5);
+		const color10 = lighten(values, 10);
+		const color20 = lighten(values, 20);
 
-// 	const renderBody = () => {
-// 		document.body.style.width = 'calc(100% - 180px)';
-// 	};
+		const colorDark5 = lighten(values, -5);
+		const colorDark10 = lighten(values, -10);
+		const colorDark20 = lighten(values, -20);
 
-// 	const renderSidebar = () => {
-// 		const node = document.createElement('div');
-// 		node.style.position = 'fixed';
-// 		node.style.right = '0';
-// 		node.style.top = '0';
-// 		node.style.bottom = '0';
-// 		node.style.borderLeft = '1px solid #ccc';
-// 		node.style.zIndex = '999';
-// 		node.style.width = '180px';
-// 		node.style.padding = '60px 20px';
-// 		node.style.background = 'white';
-// 		node.style.boxSizing = 'border-box';
-// 		node.style.overflowY = 'auto';
+		setCssVariable('--wp-ui--color-primary', primaryColor);
 
-// 		const fieldPaddingInput = createInput({
-// 			label: 'Input: Padding',
-// 			prop: 'field-padding',
-// 			value: '0 8px',
-// 		});
+		setCssVariable('--wp-ui--color-primary-5', color5);
+		setCssVariable('--wp-ui--color-primary-10', color10);
+		setCssVariable('--wp-ui--color-primary-20', color20);
 
-// 		node.appendChild(fieldPaddingInput);
+		setCssVariable('--wp-ui--color-primary-dark-5', colorDark5);
+		setCssVariable('--wp-ui--color-primary-dark-10', colorDark10);
+		setCssVariable('--wp-ui--color-primary-dark-20', colorDark20);
+	}
 
-// 		document.body.appendChild(node);
-// 	};
+	function createPrimaryColorObserver() {
+		let __currentPrimaryColor = getCurrentPrimaryColor();
+		const observer = new MutationObserver(function(mutations) {
+			mutations.forEach(function(mutation) {
+				if (mutation.attributeName === 'style') {
+					if (__currentPrimaryColor !== getCurrentPrimaryColor()) {
+						setColorVariants();
+						__currentPrimaryColor = getCurrentPrimaryColor();
+					}
+				}
+			});
+		});
 
-// 	const initialize = () => {
-// 		console.log('Design Experiments!');
-// 		renderBody();
-// 		renderSidebar();
-// 	};
+		// Notify me of style changes
+		const observerConfig = {
+			attributes: true,
+			attributeFilter: ['style'],
+		};
+		const targetNode = document.documentElement;
+		observer.observe(targetNode, observerConfig);
+	}
 
-// 	window.onload = () => {
-// 		initialize();
-// 	};
-// })();
+	requestAnimationFrame(() => {
+		setColorVariants();
+		createPrimaryColorObserver();
+	});
+})();
